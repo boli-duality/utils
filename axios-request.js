18864a1677cf276f9a1292e31f9ea38aaa7fa563
boliyou <2454462834@qq.com>
2021-12-07 16:19:17 +0800

    新增axios请求封装

---
+++ axios-request.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+// import { Message } from 'element-ui'
+// import store from '@/store'
+// import router from '@/router'
+
+// create an axios instance
+const service = axios.create({
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: '/api',
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+// service.interceptors.request.use(
+//   config => {
+//     // do something before request is sent
+
+//     if (store.getters.token) {
+//       // let each request carry token
+//       // ['X-Token'] is a custom headers key
+//       // please modify it according to the actual situation
+//       config.headers['Authorization'] = `Bearer ${store.getters.token}`
+//     }
+//     return config
+//   },
+//   error => {
+//     // do something with request error
+//     console.log(error) // for debug
+//     return Promise.reject(error)
+//   }
+// )
+
+// response interceptor
+// service.interceptors.response.use(
+//   /**
+//    * If you want to get http information such as headers or status
+//    * Please return  response => response
+//    */
+
+//   /**
+//    * Determine the request status by custom code
+//    * Here is just an example
+//    * You can also judge the status by HTTP Status Code
+//    */
+//   response => {
+//     const { success, data, message } = response.data
+
+//     if (success) {
+//       return data
+//     } else {
+//       Message.error(message)
+//       return Promise.reject(new Error('请求数据失败!'))
+//     }
+//   },
+//   error => {
+//     console.log('err' + error) // for debug
+//     Message({
+//       message: error.message,
+//       type: 'error',
+//       duration: 5 * 1000
+//     })
+//     return Promise.reject(error)
+//   }
+// )
+
+export default service
