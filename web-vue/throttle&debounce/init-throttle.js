94b2cdc68c330e912d6180c945e60555f6268d08
boliyou <2454462834@qq.com>
2022-02-15 15:53:31 +0800

    优化了vue版本的初始化节流函数

--- web-vue/throttle&debounce/init-throttle.js
+++ web-vue/throttle&debounce/init-throttle.js
@@ -4,28 +4,28 @@
  * @property {Boolean} defaultImmediately = [true] 默认延迟时间
  * @returns {Function} vue版的节流函数
  */
- function initThrottle({ delay = 500, immediately = true } = {}) {
+function initThrottle({ delay = 500, immediately = true } = {}) {
   const defaultDelay = delay
   const defaultImmediately = immediately
   const ID = Symbol('defaultID')
   const isDoThrottle = { [ID]: false }
   /**
    * vue版的节流函数
-   * @param {String} fnName 函数名字符串
+   * @param {Function} fn 需要节流的函数
    * @param {Object} options 配置选项
-   * @property {Array} params of options 执行节流的函数的参数
+   * @property {Array} params of options 需要节流的函数的参数
    * @property {Number} delay of options 延迟时间
    * @property {Boolean} immediately of options 是否立即执行
    * @property {String|Number} id of options 传入id启用不同的节流函数
    */
-  return function throttle(fnName, { params, delay, immediately, id = ID } = {}) {
-    if (typeof this[fnName] != 'function') return console.error('Not the name of a function');
+  return function throttle(fn, { params, delay, immediately, id = ID } = {}) {
+    if (typeof fn !== 'function') return console.error('[throttle warn]: TypeError: The First parameter is not a function')
     if (isDoThrottle[id]) return
     const isImmediately = immediately ?? defaultImmediately
     isDoThrottle[id] = true
-    isImmediately && this[fnName].apply(this, params)
+    isImmediately && fn.apply(this, params)
     setTimeout(() => {
-      !isImmediately && this[fnName].apply(this, params)
+      !isImmediately && fn.apply(this, params)
       isDoThrottle[id] = false
     }, delay ?? defaultDelay);
   }
