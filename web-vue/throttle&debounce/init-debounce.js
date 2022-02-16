3624189e8b87e61dd53547259a963125d4640672
boliyou <2454462834@qq.com>
2022-02-16 16:25:22 +0800

    优化了变量名

--- web-vue/throttle&debounce/init-debounce.js
+++ web-vue/throttle&debounce/init-debounce.js
@@ -2,12 +2,12 @@
  * vue版的防抖函数初始化函数
  * @author 游博林 @2022-2-16 1:42
  * @property {Number} delay = [500] 默认防抖延迟时间
- * @property {Boolean} immediately = [true] 是否立即执行
+ * @property {Boolean} immediate = [true] 是否立即执行
  * @returns {Function} debounce vue版的防抖函数
  */
-function initDebounce({ delay = 1000, immediately = true } = {}) {
+function initDebounce({ delay = 1000, immediate = true } = {}) {
   const defaultDelay = delay
-  const defaultImmediately = immediately
+  const defaultImmediate = immediate
   const ID = Symbol('defaultID')
   const timeoutID = {}
   /**
@@ -16,17 +16,17 @@ function initDebounce({ delay = 1000, immediately = true } = {}) {
    * @param {Object} options 配置选项
    * @property {Array} params of options 需要防抖的函数的参数
    * @property {Number} delay of options 防抖延迟时间
-   * @property {Boolean} immediately of options 是否立即执行
+   * @property {Boolean} immediate of options 是否立即执行
    * @property {String|Number} id of options 传入id启用不同的防抖函数
    */
-  return function debounce(fn, { params, delay, immediately, id = ID } = {}) {
+  return function debounce(fn, { params, delay, immediate, id = ID } = {}) {
     if (typeof fn !== 'function') return console.error('[throttle warn]: TypeError: The First parameter is not a function')
-    const isImmediately = immediately ?? defaultImmediately
-    if (isImmediately && !timeoutID[id]) fn.apply(this, params)
+    const isimmediate = immediate ?? defaultImmediate
+    if (isimmediate && !timeoutID[id]) fn.apply(this, params)
     clearTimeout(timeoutID[id])
     timeoutID[id] = setTimeout(() => {
-      if (!isImmediately) fn.apply(this, params)
-      if (isImmediately) timeoutID[id] = void 0
+      if (!isimmediate) fn.apply(this, params)
+      if (isimmediate) timeoutID[id] = void 0
     }, delay ?? defaultDelay)
   }
 }
