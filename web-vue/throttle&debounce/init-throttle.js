2aff40dbe45a1a5aae79f7fdbd9ef7dc119a02de
boli-duality <60964870+boli-duality@users.noreply.github.com>
2022-06-08 10:36:23 +0800

    Update init-throttle.js

--- web-vue/throttle&debounce/init-throttle.js
+++ web-vue/throttle&debounce/init-throttle.js
@@ -1,8 +1,8 @@
 /**
  * vue版的节流函数初始化函数
  * @author 游博林 @2022-2-15 18:56
- * @property {Number} delay = [500] 默认节流延迟时间
- * @property {Boolean} immediate = [true] 是否立即执行
+ * @property {Number} delay = 500 默认节流延迟时间
+ * @property {Boolean} immediate = true 是否立即执行
  * @returns {Function} vue版的节流函数
  */
  function initThrottle({ delay = 500, immediate = true } = {}) {
